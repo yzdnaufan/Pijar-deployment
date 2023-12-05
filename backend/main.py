81608774e0104129b1bddfd46f6aea7042a795c5
yzdnaufan <ahyaznau@gmail.com>
2023-12-05 19:48:22 +0700

    update cors

--- backend/main.py
+++ backend/main.py
@@ -28,9 +28,9 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
